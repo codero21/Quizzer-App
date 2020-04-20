94cbc31e279823837c8f8ae76d76926d34989064
Rollin Francois <rfrancois21@hotmail.com>
2020-04-20 06:40:10 -0400

    Connect UI with ViewController

--- Quizzer/ViewController.swift
+++ Quizzer/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    }
+    
 }
+
 
